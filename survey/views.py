2529140b4269848f5319221b64504256e465b51a
Rafi M Feroze <mohamed.feroze@monash.edu>
2016-05-11 12:19:58 +1000

    add login required

--- survey/views.py
+++ survey/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 # index home page
 def index(request):
     context_dict = {}
     return render(request, 'sleepvl/index.html', context_dict)
 
-
+@login_required
 def site_admin(request):
     context_dict = {}
-    return render(request,'sleepvl/site_admin.html', context_dict)
+    #return render(request,'sleepvl/site_admin.html', context_dict)
+    return HttpResponse('<H3>Access Denied for {}: Not implemented <H3><BR>'.format(repr(request.user)))
