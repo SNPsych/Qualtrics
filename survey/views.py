06d9d147d4567d1936a0c66c1c1793831b99ef81
Rafi M Feroze <mohamed.feroze@monash.edu>
2016-05-11 14:27:39 +1000

    custom http header

--- survey/views.py
+++ survey/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-import pprint
+#import pprint
 
 # index home page
 def index(request):
     context_dict = {}
     return render(request, 'sleepvl/index.html', context_dict)
 
-@login_required
 def site_admin(request):
     context_dict = {}
     #return render(request,'sleepvl/site_admin.html', context_dict)
-    msgStr = '<H3>Access Denied for {}: Not implemented <H3><BR>'.format(repr(request.user))
+    msgStr = 'User is authenticated {}'.format(repr(request.user))
+    if not request.user.is_authenticated():
+        msgStr = '<H3>Access Denied for {}: Not implemented <H3><BR>'.format(repr(request.user))
     print(msgStr)
     return HttpResponse(msgStr)
 
@@ -21,21 +22,22 @@ from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.utils.http import is_safe_url
 # additional imports
 
-def login(request, template_name='activity/login.html',
-        redirect_field_name=REDIRECT_FIELD_NAME):
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint('user is ')
-    pp.pprint(request.user)
-    if hasattr(request, 'user') and request.user.is_authenticated():
-        redirect_to = request.POST.get(redirect_field_name,
-            request.GET.get(redirect_field_name, ''))
-        #if not is_safe_url(url=redirect_to, host=request.get_host()):
-         #   redirect_to = resolve_url(settings.LOGIN_REDIRECT_URL)
-        pp.pprint('redirecting to ' + redirect_to)
-        return HttpResponseRedirect(redirect_to)
-    pp.pprint('trying login auth_login')
-    return auth_login(request, template_name = template_name,
-        redirect_field_name = redirect_field_name)
+# def login(request, template_name='activity/login.html',
+#         redirect_field_name=REDIRECT_FIELD_NAME):
+#     pp = pprint.PrettyPrinter(indent=4)
+#     pp.pprint('user is ')
+#     pp.pprint(request.user)
+#     if hasattr(request, 'user') and request.user.is_authenticated():
+#         redirect_to = request.POST.get(redirect_field_name,
+#             request.GET.get(redirect_field_name, ''))
+#         #if not is_safe_url(url=redirect_to, host=request.get_host()):
+#          #   redirect_to = resolve_url(settings.LOGIN_REDIRECT_URL)
+#         pp.pprint('redirecting to ' + redirect_to)
+#         return HttpResponseRedirect(redirect_to)
+#     pp.pprint('trying login auth_login')
+#     return auth_login(request, template_name = template_name,
+#         redirect_field_name = redirect_field_name)
+#
 
 from django.contrib.auth.middleware import RemoteUserMiddleware
 
